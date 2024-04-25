f6a599bf3c47adc1f80b3f6e522fab70e0d97226
NgunyiGachie <antogachie@gmail.com>
2024-04-25 08:13:47 +0300

    Complete lab

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About({ image, about }) {
+    
+    const placeholderImage = "https://via.placeholder.com/215";
+
+    return (
+        <aside>
+            <img src={image || placeholderImage} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    )
+}
+
+export default About;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({ blogData }) {
+    return (
+        <main>
+            {blogData.posts.map(post => (
+                <Article
+                    key={post.id}
+                    title={post.title}
+                    preview={post.preview}
+                    date={post.date}
+                />
+            ))}
+        </main>
+    );
+}
+
+export default ArticleList;
+
